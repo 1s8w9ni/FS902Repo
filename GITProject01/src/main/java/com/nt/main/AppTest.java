f8e2a81079704a94047c1e6d2801c46797770d52
1s8w9n9 <ashwinigoureddygari@gmail.com>
2024-02-28 09:39:44 +0530

    commit

---
+++ GITProject01/src/main/java/com/nt/main/AppTest.java
@@ -0,0 +1,16 @@
+package com.nt.main;
+public class AppTest 
+{
+   public int sum(int x,int y)
+   {
+	   return x+y;
+   }
+   public int sub(int x,int y)
+   {
+	   return x-y;
+   }
+   public int mul(int x,int y)
+   {
+	   return x*y;
+   }
+}
